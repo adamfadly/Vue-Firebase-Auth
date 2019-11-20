be55db803728c87af601c3917407450e04dc5414
adam fadly <adamfadly11@gmail.com>
2019-11-20 12:33:14 +0700

    create authentication with firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyBDsxQ6-8Lu8DGHBWQbgII7EddLs5inpo4",
+  authDomain: "vue-firebase-tutorial-e3558.firebaseapp.com",
+  databaseURL: "https://vue-firebase-tutorial-e3558.firebaseio.com",
+  projectId: "vue-firebase-tutorial-e3558",
+  storageBucket: "vue-firebase-tutorial-e3558.appspot.com",
+  messagingSenderId: "846506298136",
+  appId: "1:846506298136:web:5bbdd0abf3f121bd182c6d",
+  measurementId: "G-SV7RC5EB5W"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
---
+++ src/views/SignUp.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="sign-up">
+        <p>let's create a new !</p>
+        <input type="text" v-model="email" placeholder="Email"><br>
+        <input type="password" v-model="password" placeholder="Password"><br>
+        <button @click="signUp">Sign Up</button>
+        <span>or go back to <router-link to="/login">Login</router-link>.</span>
+    </div>    
+</template> 
+
+<script>
+import firebase from 'firebase';
+
+export default {
+    name: 'SignUp',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        signUp: function() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then (
+                function () {
+                    alert('your account has been created!')
+                },
+                function (err) {
+                    alert('Oops' + err.message)
+                }
+            );
+        }
+    }
+}
+</script>
+
+<style scooped>
+
+    .signUp {
+        margin-top: 40px;
+    }
+
+    input{
+        margin: 10px 0;
+        width: 10%;
+        cursor: pointer;
+    }
+
+    button {
+        margin-top: 10px;
+        width: 10%;
+        font-size: 11px;
+    }
+
+</style>
